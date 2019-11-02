9f45669794ca7d21951d8aac5a6c3223dad9c92b
Ahmed Dev <a.ahmed9316@gmail.com>
2019-11-02 13:41:09 -0400

    grasshopper

---
+++ grasshopperRooms.js
@@ -0,0 +1,23 @@
+// You are creating an escape the room game.
+// The first step is create an object called rooms that contains all of the rooms of the game.
+// The room object should have at least 3 rooms as properties, and each individial room should have at least three properties (example: name, description, completed).
+
+
+
+var rooms = {
+room1:{
+name:'Room1',
+description:"green",
+completed:"yes",
+},
+room2:{
+name:'Room2',
+description:'blue',
+completed:'yes',
+},
+room3:{
+name:'Room3',
+description:'red',
+completed:'yes'
+}
+}
