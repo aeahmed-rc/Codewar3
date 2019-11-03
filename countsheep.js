636189fa2b53f58c7a1d5748ab30499adfd95d9b
Ahmed Dev <a.ahmed9316@gmail.com>
2019-11-03 12:57:24 -0500

    countsheep

---
+++ countsheep.js
@@ -0,0 +1,15 @@
+
+// Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present 
+// in the array (true means present).
+
+
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  var num = 0;
+
+  for(var i = 0; i < arrayOfSheep.length; i++)
+    if(arrayOfSheep[i] == true)
+      num++;
+
+  return num;
+}
