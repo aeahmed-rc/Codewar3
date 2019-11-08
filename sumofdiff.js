86eaf4a4075982fffe57cfdf7039f6905f6921ca
Ahmed Dev <a.ahmed9316@gmail.com>
2019-11-07 23:57:03 -0500

    sumofdiff

---
+++ sumofdiff.js
@@ -0,0 +1,11 @@
+Sum of differences in array
+
+
+function sumOfDifferences(arr) {
+arr.sort(function(a, b){return b-a});
+let sum=0
+for (let i=0;i<arr.length-1;i++){
+sum+=arr[i]-arr[i+1]
+}
+return sum
+}
