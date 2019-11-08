20276e707094d6631f0da6672e6064f5db3263d4
Ahmed Dev <a.ahmed9316@gmail.com>
2019-11-08 10:49:48 -0500

    enoughspace

---
+++ enoughspace.js
@@ -0,0 +1,19 @@
+// Task Overview:
+// You have to write a function that accepts three parameters:
+//
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus.
+// wait is the number of people waiting to get on to the bus.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+
+function enough(cap, on, wait){
+if(on+wait>cap){
+return (on+wait)-cap
+}else{
+return 0
+}
+
+
+
+}
