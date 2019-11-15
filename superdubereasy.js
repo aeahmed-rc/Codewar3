d2a97e5ba72ae98451bff74b857e16c9415e99a8
Ahmed Dev <a.ahmed9316@gmail.com>
2019-11-14 22:24:11 -0500

    supereasy

---
+++ superdubereasy.js
@@ -0,0 +1,6 @@
+// Make a function that returns the value multiplied by 50 and increased by 6.
+// If the value entered is a string it should return "Error".
+
+function problem(x){
+let math = x; if(typeof x === "number"){ return (x*50)+6 }else{ return 'Error' }
+}
