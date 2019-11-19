c659a47c5aef2ac6a5c3a15e07e2daed75f89c27
Ahmed Dev <a.ahmed9316@gmail.com>
2019-11-19 09:56:51 -0500

    removeanch

---
+++ removenchor.js
@@ -0,0 +1,16 @@
+Remove anchor from URL
+
+function removeUrlAnchor(url){
+console.log(url)
+let rem='#'
+
+return url.split(rem,1)[0]
+// return url.slice(0,16)
+  // TODO: complete
+}
+// another way to do this
+// text = 'some string... this part will be removed.'
+// head, sep, tail = text.partition('...')
+//
+// >>> print head
+// some string
