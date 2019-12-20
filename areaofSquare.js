c5cc4dc092b144fee81a2d7df09b1e41a80d5e4a
Ahmed Dev <a.ahmed9316@gmail.com>
2019-12-20 14:32:52 -0500

    squares

---
+++ areaofSquare.js
@@ -0,0 +1,13 @@
+
+Problem: Area Of Square
+
+A=1/4 of the circumference so
+A=2* pi*r/4
+4A=2*pi*r
+areaofsquare=a^2 which is the length times width
+aka the radius *radius will give us area of square
+function squareArea(A){
+
+return +((2*A/Math.PI)*(2*A/Math.PI)).toFixed(2)
+
+}
