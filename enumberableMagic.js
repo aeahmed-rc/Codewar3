8888cd6e2dac85b5473a9618228d60f6a3762e77
Ahmed Dev <a.ahmed9316@gmail.com>
2020-01-18 14:17:27 -0500

    enumberable

---
+++ enumberableMagic.js
@@ -0,0 +1,15 @@
+function take(arr, n) {
+console.log(arr,n)
+let array=[]
+
+if ( arr === array){
+console.log(array)
+return array
+
+}else {
+for(let i = 0 ; i<n ; i++){
+array.push(arr[i])
+}
+ return array
+}
+}
