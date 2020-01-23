35a9bb205b70c1adedf39234b25cdc1f410319f5
Ahmed Dev <a.ahmed9316@gmail.com>
2020-01-22 22:26:47 -0500

    reverseNumber

---
+++ reversenumber.js
@@ -0,0 +1,14 @@
+function digitize(n) {
+let str = n.toString()
+console.log(str)
+let sp = str.split('').reverse()
+console.log(sp)
+return sp.map( x => Number(x))
+
+  //code here
+}
+// params: number
+// return: the numbers reversed and in an array
+// ex: 123 return [3,2,1]
+// psudo :get the numbers to be placeed into an array reverse them so that last number is first
+// and then return that
