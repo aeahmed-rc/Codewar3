1c5c19e7b98f5e48ff4e42fbd16a0c128a751245
Ahmed Dev <a.ahmed9316@gmail.com>
2020-02-09 10:36:52 -0500

    findng total number of angle sum

---
+++ angles.js
@@ -0,0 +1,17 @@
+function angle(n) {
+console.log(n)
+sum =60
+ for (let i = 0 ; i <n ;i++){
+ sum = sum +
+ }
+}
+// p-number of angles
+// return total sum of all the angles
+// ex = 3 sides each side = 60
+//      4 sides each side = 90
+//      p= take the number of sides every time it goes up each angle increases by 30
+//      n
+3 = 180 3*60=180
+4=360 4 * 90= 360
+5=540 5 * 120=540
+n * 
