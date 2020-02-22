8edfaa00f721a351af601518f203b124afd01f6d
Ahmed Dev <a.ahmed9316@gmail.com>
2020-02-22 13:25:10 -0500

    gcd sum codewar

--- GCDSum.js
+++ GCDSum.js
@@ -1,6 +1,9 @@
 // Given the sum and gcd of two numbers, return those two numbers in ascending order.
 // If the numbers do not exist, return -1, (or return NULL in C).
 // GCD sum
+
+
+
 function solve(s,g){
 let num=s-g
 let arr = [g]
