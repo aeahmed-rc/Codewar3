b74b4ccd60408703a4d2a21b09cf6bce46e4570f
Ahmed Dev <a.ahmed9316@gmail.com>
2020-03-11 08:49:04 -0400

    im not sure why the loop doesnt work

--- squarearraynum.js
+++ squarearraynum.js
@@ -1,17 +1,17 @@
 function index(array, n){
-let num=1;
-let square;
-for (let i = 0;i<array.length-1;i++){
+let num=0;
+let square=-1;
+for (let i = 0;i<=array.length;i++){
 num++
+console.log(num)
 if (num === n){
-square=Math.sqrt(array[i])
+ square=Math.sqrt(array[i])
 console.log(square)
-} else if (n > array.length){
-return -1
 }
+ return square
 }
 }
 [1,2,3]
 loop through the array everytime it loops increase number so we know how many loops it took
-then 
+then
 n=2 so at 3 take that number an square it
