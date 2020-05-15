0622c52723b2d972fb97c58781ab361d3102b6e3
Ahmed Dev <a.ahmed9316@gmail.com>
2020-05-14 23:37:12 -0400

    removed colons

--- initials.js
+++ initials.js
@@ -1,6 +1,6 @@
 function abbrevName(name)s{
     var names = name.split(' '),
-        initials = names[0].substring(0, 1).toUpperCase() + ".";
+        initials = names[0].substring(0, 1).toUpperCase() + "."
     console.log(initials)
     if (names.length > 1) {
         initials += names[names.length - 1].substring(0, 1).toUpperCase();
