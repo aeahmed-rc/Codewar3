15e41ae4b6e778b7a79495cae2c349e367e01ab5
Ahmed Dev <a.ahmed9316@gmail.com>
2020-05-16 22:59:46 -0400

    brackets removed

--- initials.js
+++ initials.js
@@ -2,7 +2,7 @@ function abbrevName(name)s{
     var names = name.split(' '),
         initials = names[0].substring(0, 1).toUpperCase() + "."
     console.log(initials)
-    if (names.length > 1) {
+    if (names.length > 1) 
         initials += names[names.length - 1].substring(0, 1).toUpperCase()
     console.log(initials)
     return initials
