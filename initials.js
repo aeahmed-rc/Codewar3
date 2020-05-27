605d3c6a93f9fbda86ad328e6ba0cc9cac220a74
Ahmed Dev <a.ahmed9316@gmail.com>
2020-05-26 22:46:26 -0400

    deleted

--- initials.js
+++ initials.js
@@ -2,7 +2,5 @@ function abbrevName(name)s{
     var names = name.split(' '),
         initials = names[0].substring(0, 1).toUpperCase() + "."
     console.log(initials)
-    if 
-        initials += names[names.length - 1].substring(0, 1).toUpperCase()
-    console.log(initials)
-    return initials
+    if
+    
