f7ce07c2a564167ddc90db94427ae7362e7f15b2
Ahmed Dev <a.ahmed9316@gmail.com>
2020-09-18 16:32:47 -0400

    r

--- removezero.js
+++ removezero.js
@@ -8,7 +8,7 @@
 // For inputArray = [5, 1, 2, 3, 0, 1, 5, 0, 2], the output should be 11.
 // The answer was obtained as 5 + 1 + 2 + 3 = 11
 // Input
-// array inputA
+// array input
 // Constraints: 5 ≤ inputArray.length ≤ 50, 0 ≤ inputArray[i] ≤ 10.
 // [output] an integer
 function houseNumbersSum(inputArray) {
