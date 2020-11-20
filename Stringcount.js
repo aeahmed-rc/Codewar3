2b26d78faff2ff1307f01e44c826deaf8c326f70
Ahmed Dev <a.ahmed9316@gmail.com>
2020-11-19 23:37:03 -0500

    comment

--- Stringcount.js
+++ Stringcount.js
@@ -4,4 +4,4 @@ console.log(spl)
 console.log(Math.min(...spl))
 return Math.min(...spl)
 }
-// function to count the number of words in a string.
+// function to count the number of words in a string...
